729c399c6779c4b5ec91e1a76275684907b2a34e
Olle Bröms <olle.broms@ewebbyran.se>
2023-08-31 10:04:44 +0200

    fix: added isServerSide check and usage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { isServerSide } from '@/utils/detect-side'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  console.log(isServerSide ? 'SERVER SIDE CODE' : 'CLIENT SIDE CODE');
+
   return (
     <>
       <Head>
